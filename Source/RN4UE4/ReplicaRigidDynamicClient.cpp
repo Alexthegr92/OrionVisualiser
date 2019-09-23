c51858664cf73b4b6ed7698f47628317e9e5393d
alexther92 <alexthegr92@gmail.com>
2019-09-23 13:59:59 +0100

    Fixed: Replicas actually get properly destroyed

--- Source/RN4UE4/ReplicaRigidDynamicClient.cpp
+++ Source/RN4UE4/ReplicaRigidDynamicClient.cpp
@@ -136,7 +136,13 @@ void UReplicaRigidDynamicClient::UpdateTransform()
 
 bool UReplicaRigidDynamicClient::DeserializeDestruction(BitStream *destructionBitstream, Connection_RM3 *sourceConnection)
 {
+	if (visual != nullptr)
+	{
+		visual->Destroy();
+	}
+
 	GetOwner()->Destroy();
+
 	return true;
 }
 
